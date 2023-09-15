cee3bfa366d250dea8683f62c7338dfba8746f5d
hhtetwei <hhtet263510@gmail.com>
2023-09-15 16:46:18 +0630

    still in wip

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,41 +1,18 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/prop-types */
-import {
-  ArticleOutlined,
-  // Groups,
-  // PeopleOutlined,
-  PublishOutlined,
-} from '@mui/icons-material';
-import { Box, Paper, Tab, Tabs, makeStyles } from '@mui/material';
+import { Box, Paper, Tab, Tabs } from '@mui/material';
 import { useAuth } from '../hooks/useAuth';
-import { Link, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { ROLES } from '../constants/roles';
+import ListAltIcon from '@mui/icons-material/ListAlt';
+import TabIcon from '@mui/icons-material/Tab';
+import LocalPostOfficeOutlinedIcon from '@mui/icons-material/LocalPostOfficeOutlined';
 import React from 'react';
-
-const Item = ({ title, to, icon }) => {
-  const location = useLocation();
-  const isActive = location.pathname.includes(to);
-
-  return (
-    <Link to={to} style={{ textDecoration: 'none' }}>
-      {/* <Tabs
-        variant="contained"
-        sx={{ borderRadius: '50px', display: 'flex', gap: 1 }}
-        color={isActive ? 'primary' : 'secondary'}
-      >
-        {' '}
-        {icon} {title}
-      </Tabs> */}
-    </Link>
-  );
-};
 
 const Navbar = () => {
   const { user } = useAuth();
 
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (event, newValue) => {
+  const handleChange = (newValue) => {
     setValue(newValue);
   };
 
@@ -43,13 +20,10 @@ const Navbar = () => {
     <Box display="flex" gap="10px">
       {user?.role === ROLES.SUPER_ADMIN ? (
         <>
-          {/* <Item title="Users" to="/users" icon={<PeopleOutlined />} />
-          <Item title="Work Flows" to="/workflows" icon={<Groups />} />
-          <Item title="Departments" to="/departments" icon={<Groups />} /> */}
-
           <Paper
             style={{
               display: 'flex',
+              width: 'auto',
             }}
           >
             <Tabs
@@ -60,21 +34,71 @@ const Navbar = () => {
               onChange={handleChange}
               centered
             >
-              <Tab label="Item One" />
-              <Tab label="Item Two" />
-              <Tab label="Item Three" />
+              <Tab
+                component={Link}
+                label="Users"
+                to="/users"
+                icon={<ListAltIcon />}
+                iconPosition="start"
+              />
+              <Tab
+                component={Link}
+                label="Work Flows"
+                to="/workflows"
+                icon={<TabIcon />}
+                iconPosition="start"
+              />
+              <Tab
+                component={Link}
+                label="Departments"
+                to="/departments"
+                icon={<LocalPostOfficeOutlinedIcon />}
+                iconPosition="start"
+              />
             </Tabs>
           </Paper>
         </>
       ) : (
         <>
-          <Item title="All Requests" to="/all" icon={<PublishOutlined />} />
+          {/* <Item title="All Requests" to="/all" icon={<PublishOutlined />} />
           <Item
             title="My Requests"
             to="/my-requests"
             icon={<PublishOutlined />}
           />
-          <Item title="Inbox" to="/inbox" icon={<ArticleOutlined />} />
+          <Item title="Inbox" to="/inbox" icon={<ArticleOutlined />} /> */}
+
+          <Tabs>
+            <Tab
+              component={Link}
+              label="All Requests"
+              to="/users"
+              icon={<ListAltIcon />}
+              iconPosition="start"
+            />
+            <Tab
+              component={Link}
+              label="My Requests"
+              to="/workflows"
+              icon={<TabIcon />}
+              iconPosition="start"
+            />
+            <Tab
+              component={Link}
+              label="Inbox"
+              to="/departments"
+              icon={<LocalPostOfficeOutlinedIcon />}
+              iconPosition="start"
+            />
+
+            <Tab
+              component={Link}
+              label="To Acknowledge"
+              to="/departments"
+              icon={<LocalPostOfficeOutlinedIcon />}
+              iconPosition="start"
+            />
+          </Tabs>
         </>
       )}
     </Box>
