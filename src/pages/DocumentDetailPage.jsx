db9f568f82950cba866c680179482d8415abdd0f
ThaeNandarSeint <dardar2003nandar@gmail.com>
2023-12-13 11:17:42 +0630

    if case closed, edit button will not be shown

--- src/pages/DocumentDetailPage.jsx
+++ src/pages/DocumentDetailPage.jsx
@@ -153,7 +153,8 @@ const DocumentDetailPage = () => {
                   variant="contained"
                   color="primary"
                 />
-                {user?._id === document?.payload?.currentReviewer &&
+                {!document.payload.isCaseClosed &&
+                  user?._id === document?.payload?.currentReviewer &&
                   user?.permissions?.canPrepare && (
                     <LinkButton
                       width="200px"
