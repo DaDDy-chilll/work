ca01185edbf9e1332bd22e215173e29170e27d7c
ThaeNandarSeint <thaenandar14seint@gmail.com>
2024-08-22 14:51:14 +0630

    show alert for disabled workflows

--- src/components/row/WorkflowRow.jsx
+++ src/components/row/WorkflowRow.jsx
@@ -4,6 +4,7 @@ import { StyledTableCell, StyledTableRow } from '../styled';
 import WorkflowRoute from '../ui/WorkflowRoute';
 import { getDepartmentsFromWorkflow } from '../../helpers';
 import { TableActionButton } from '@/components';
+import { Error } from '@mui/icons-material';
 
 const WorkflowRow = ({ payload }) => {
   return (
@@ -11,7 +12,12 @@ const WorkflowRow = ({ payload }) => {
       {payload &&
         payload.map((data) => (
           <StyledTableRow key={data?._id}>
-            <StyledTableCell>{data?.groupId}</StyledTableCell>
+            <StyledTableCell>
+              <div className="flex gap-1 items-center">
+                {data?.isDisabled && <Error className="text-xl text-red-500" />}
+                <span>{data?.groupId}</span>
+              </div>
+            </StyledTableCell>
             <StyledTableCell>{data?.name}</StyledTableCell>
             <StyledTableCell>{data?.description}</StyledTableCell>
             <StyledTableCell>
